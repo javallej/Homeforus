b4b9370bdbe630bdf00bb686f749689555b01493
Joseph Vallejo <javallej@asu.edu>
2021-09-26 12:49:57 -0400

    Consumer Update

--- src/main/java/homeforus/core/ConsumerList.java
+++ src/main/java/homeforus/core/ConsumerList.java
@@ -1,5 +1,70 @@
 package main.java.homeforus.core;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConsumerList {
+
+    public List<ConsumerListObject> List(int Consumer_ID) throws SQLException, IOException {
+        ResultSet rs = null;
+        PreparedStatement stmt = null;
+        
+        Connection connect = DBConnect.connect(Setup.setup().get("jdbcUrl"),Setup.setup().get("jdbcUser"), Setup.setup().get("jdbcPasswd"),
+                Setup.setup().get("jdbcDriver"));
+        
+        List <ConsumerListObject> consumerinformation = new ArrayList<ConsumerListObject>();
+
+        try {
+
+
+            String query = "SELECT * FROM CONSUMER WHERE CONSUMER.Consumer_ID = ?";
+
+            stmt = connect.prepareStatement(query);
+            stmt.setInt(1, Consumer_ID);
+            rs = stmt.executeQuery();
+
+            
+            while (rs.next()) {
+                ConsumerListObject cobject = new ConsumerListObject();
+                cobject.setConsumerID(rs.getInt(1));
+                cobject.setConsumerUsername(rs.getString(2));
+                cobject.setDOB(rs.getString(3));
+                cobject.setSSN(rs.getInt(4));
+                consumerinformation.add(cobject);
+            }
+            
+            return consumerinformation;
+            
+            
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+
+        finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+
+                if (connect != null) {
+                    connect.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+
+        }
+        return consumerinformation;
+    }
+
+    
 
 }
--- src/main/java/homeforus/core/ConsumerListObject.java
+++ src/main/java/homeforus/core/ConsumerListObject.java
@@ -1,5 +1,42 @@
 package main.java.homeforus.core;
 
 public class ConsumerListObject {
+    
+    private int consumerid;
+    private String consumer_username;
+    private String dob;
+    private int ssn;
+    
+    public void setConsumerID(int Consumerid) {
+        this.consumerid = Consumerid;
+    }
 
+    public int getConsumerID() {
+        return this.consumerid;
+    }
+    
+    public void setConsumerUsername(String Consumer_username) {
+        this.consumer_username = Consumer_username;
+    }
+
+    public String getConsumerUsername() {
+        return this.consumer_username;
+    }
+    
+    public void setDOB(String Dob) {
+        this.dob = Dob;
+    }
+
+    public String getDOB() {
+        return this.dob;
+    }
+    
+    public void setSSN(int Ssn) {
+        this.ssn = Ssn;
+    }
+
+    public int getSSN() {
+        return this.ssn;
+    }
 }
+
