225abfdab87aeb0d1190c0b5ae9dfbd90ef9ae90
Jack Norman <jlnorma4@asu.edu>
2021-10-02 21:43:41 -0500

    DetailPanel and subclasses finished- I think

--- src/main/java/homeforus/gui/DetailPanel.java
+++ src/main/java/homeforus/gui/DetailPanel.java
@@ -12,18 +12,17 @@ public class DetailPanel extends JPanel {
     private JLabel row4;
     private JLabel row5;
 
-
     public DetailPanel() {
 
-        row1 = new JLabel("$123,000");
+        row1 = new JLabel("Default Row 1");
         row1.setFont(new Font("Sans_Serif",Font.BOLD,20));
-        row2 = new JLabel("2 bds 1 ba 2 floors 1,500 sqft");
+        row2 = new JLabel("Default Row 2");
         row2.setFont(new Font("Sans_Serif",Font.BOLD,14));
-        row3 = new JLabel( "742 Evergreen Terrace, Springfield, OR 12345");
+        row3 = new JLabel( "Default Row 3");
         row3.setFont(new Font("Sans_Serif", Font.PLAIN,14));
-        row4 = new JLabel("Year built: 1969");
+        row4 = new JLabel("Default Row 4");
         row4.setFont(new Font("Sans_Serif", Font.PLAIN,14));
-        row5 = new JLabel("52 days on HomeForUs");
+        row5 = new JLabel("Default Row 5");
         row5.setFont(new Font("Sans_Serif", Font.PLAIN,14));
 
         setPreferredSize(new Dimension(500,130));
@@ -33,11 +32,27 @@ public class DetailPanel extends JPanel {
         add(row3);
         add(row4);
         add(row5);
-
-        setBorder(debugBorder());
-
     }
 
+    public JLabel getRow1() {
+        return row1;
+    }
+
+    public JLabel getRow2() {
+        return row2;
+    }
+
+    public JLabel getRow3() {
+        return row3;
+    }
+
+    public JLabel getRow4() {
+        return row4;
+    }
+
+    public JLabel getRow5() {
+        return row5;
+    }
 
     public static MatteBorder debugBorder() {
         return new MatteBorder(3, 3, 3, 3, Color.CYAN);
