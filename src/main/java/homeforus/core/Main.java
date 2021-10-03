0c353be2869d0ace4f138f9493b664e3519634f4
Joseph Vallejo <javallej@asu.edu>
2021-10-03 17:02:27 -0400

    Import Images

--- src/main/java/homeforus/core/Main.java
+++ src/main/java/homeforus/core/Main.java
@@ -32,8 +32,15 @@ public class Main {
 
     public static void main(String[] args) {
         
+        String database = System.getProperty("PrepareDatabase");
+        
         try {
         
+        if(database.equals("PrepareDatabase")) {
+            PrepareDatabase data= new PrepareDatabase();
+            data.prepare(database);
+        }
+            
         if (args.length == 1) {
             if (args[0].equals("console")) {
             UIstart uiuser = new UIstart();
@@ -42,6 +49,8 @@ public class Main {
             GUIStart guistart = new GUIStart();
             guistart.start();
         }
+            
+
         }} catch (SQLException e) {
             System.out.println("Syntax is:");
             System.out.println("java -Dconfig=/path/rdbm.properties -jar homeforus-5.0.jar gui");
---
+++ src/main/java/homeforus/core/PrepareDatabase.java
@@ -0,0 +1,8 @@
+package main.java.homeforus.core;
+
+public class PrepareDatabase {
+
+    public void prepare(String filepath) {
+        System.out.println(filepath);
+    }
+}
