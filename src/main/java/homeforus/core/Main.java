3d5468610c484d50cb02f6d6f5e5de2d941262d5
Joseph Vallejo <javallej@asu.edu>
2021-10-04 09:25:03 -0400

    Removed UI Core and Updated Main to show removed UI

--- src/main/java/homeforus/core/Main.java
+++ src/main/java/homeforus/core/Main.java
@@ -11,7 +11,6 @@ package main.java.homeforus.core;
 import java.sql.SQLException;
 import java.io.IOException;
 import main.java.homeforus.gui.GUIStart;
-import main.java.homeforus.ui.UIstart;
 
 /**
  * Class: Main
@@ -34,32 +33,13 @@ public class Main {
         
         String database = System.getProperty("PrepareDatabase");
         
-        try {
-        
         if(database != null) {
             PrepareDatabase data= new PrepareDatabase();
             data.prepare(database);
             System.exit(0);
         }
-            
-        if (args.length == 1) {
-            if (args[0].equals("console")) {
-            UIstart uiuser = new UIstart();
-            uiuser.console();
-        } else if (args[0].equals("gui")) {
-            GUIStart guistart = new GUIStart();
-            guistart.start();
-        }
-            
-
-        }} catch (SQLException e) {
-            System.out.println("Syntax is:");
-            System.out.println("java -Dconfig=/path/rdbm.properties -jar homeforus-5.0.jar gui");
-            System.out.println("or");
-            System.out.println("java -Dconfig=/path/rdbm.properties -jar homeforus-5.0.jar console");
-        }
         GUIStart guistart = new GUIStart();
         guistart.start();
         }
         
-    }
+ }
